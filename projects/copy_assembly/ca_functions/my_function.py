87e75aaf8eaa08fcc32e946d3eca3a1dfc9ee4b1
Pavel Golubev <bobik0193@mail.ru>
2025-10-27 14:44:58 +0300

    decorater_set_object_name

--- projects/copy_assembly/ca_functions/my_function.py
+++ projects/copy_assembly/ca_functions/my_function.py
@@ -1,4 +1,17 @@
 from pathlib import Path
+from PyQt5 import QtWidgets
+
+
+def decorater_set_object_name(cls, *args, **kwargs):
+    def wraper(*args, **kwargs):
+        instance = cls(*args, **kwargs)
+
+        for name, widget in instance.__dict__.items():
+            if isinstance(widget, QtWidgets.QWidget):
+                if not widget.objectName():
+                    widget.setObjectName(f'{instance.__class__.__name__}_{name}')
+        return instance
+    return wraper
 
 
 def strip_path(path: str) -> str:
